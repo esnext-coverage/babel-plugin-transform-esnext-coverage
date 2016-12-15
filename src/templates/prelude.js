519e1192ea5b7812be2a210f72fb9a266d42a72d
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-12-15 17:52:57 +0100

    refactor(prelude): micro-optimisations
     - coverted `const` to `var` to reduce transformation time
     - change AssignmentExpression to UpdateExpression (less AST nodes)

--- src/templates/prelude.js
+++ src/templates/prelude.js
@@ -1,9 +1,9 @@
-const VARIABLE = (function (context) {
-  const locations = JSON.parse(LOCATIONS);
+var VARIABLE = (function (context) {
+  var locations = JSON.parse(LOCATIONS);
   context[NAMESPACE] = context[NAMESPACE] || {};
   context[NAMESPACE][FILEPATH] = locations;
   return function (id, value) {
-    locations[id][0] += 1;
+    locations[id][0]++;
     return value;
   };
 })(
